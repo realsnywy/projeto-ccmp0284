d1beb9c68be6ba8f6f629e300f508b517dfeba96
Gabriel "Snywy" Furtado <gfcleao@gmail.com>
2025-03-17 15:27:01 -0300

    mfw "intege"
    Sei digitar não, lol

--- src/main/resources/db/migration/V1__create_product_table.sql
+++ src/main/resources/db/migration/V1__create_product_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE products(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(15, 2) DEFAULT 0,
-    stock intege DEFAULT 0
+    stock INTEGER DEFAULT 0
 );
