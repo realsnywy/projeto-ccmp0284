1f9b6a8a5a7d0e9dc00f4fe228d8f76b5902ca44
Gabriel "Snywy" Furtado <gfcleao@gmail.com>
2025-03-31 15:15:38 -0300

    Cleanup

--- src/main/java/br/com/primeshoes/api/dtos/ProductDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.primeshoes.api.dtos;
-
-public record ProductDTO(String name, double price, Integer stock) {
-
-}
--- target/classes/db/migration/V2__create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    admin tinyint default(false)
-)
